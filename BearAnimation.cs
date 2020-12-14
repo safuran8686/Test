60f053bdd29f11299a7049f63e3913e966103ef9
safuran8686 <safura.nnloop0927@gmail.com>
2020-12-14 23:34:24 +0900

    initial commit

---
+++ BearAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAnimation : MonoBehaviour
+{
+    [SerializeField] float time;
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("AnimPlay", time);
+    }
+
+    void AnimPlay()
+    {
+       this.gameObject.GetComponent<Animator>().enabled = true;
+       
+        Debug.Log("anim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
